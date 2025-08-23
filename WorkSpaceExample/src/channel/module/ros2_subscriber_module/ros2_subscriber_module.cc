bd2c5bb68633c1526aed5f41d5e3dad38cae97b6
lzh <2661006892@qq.com>
2025-08-23 16:54:28 +0800

    添加ros2原生消息接口订阅者模块

--- WorkSpaceExample/src/channel/module/ros2_subscriber_module/ros2_subscriber_module.cc
+++ WorkSpaceExample/src/channel/module/ros2_subscriber_module/ros2_subscriber_module.cc
@@ -1,42 +1,61 @@
 #include "ros2_subscriber_module/ros2_subscriber_module.h"
-
+#include "aimrt_module_ros2_interface/channel/ros2_channel.h"
 #include "yaml-cpp/yaml.h"
 
-namespace example::channel::ros2_subscriber_module {
 
-bool Ros2SubscriberModule::Initialize(aimrt::CoreRef core) {
-  // Save aimrt framework handle
-  core_ = core;
+namespace example::channel::ros2_subscriber_module
+{
 
-  try {
-    // Read cfg
-    auto file_path = core_.GetConfigurator().GetConfigFilePath();
-    if (!file_path.empty()) {
-      YAML::Node cfg_node = YAML::LoadFile(std::string(file_path));
-      for (const auto& itr : cfg_node) {
-        std::string k = itr.first.as<std::string>();
-        std::string v = itr.second.as<std::string>();
-        AIMRT_INFO("cfg [{} : {}]", k, v);
-      }
+bool Ros2SubscriberModule::Initialize(aimrt::CoreRef core)
+{
+    // Save aimrt framework handle
+    core_ = core;
+
+    try {
+        // Read cfg
+        auto file_path = core_.GetConfigurator().GetConfigFilePath();
+        if (!file_path.empty()) {
+            YAML::Node cfg_node = YAML::LoadFile(std::string(file_path));
+            // 读取部署文件的话题名称
+            topic_name_ = cfg_node["channel_name"].as<std::string>();
+        }
+
+        // 获取一个订阅者句柄
+        subscriber_ = core_.GetChannelHandle().GetSubscriber(topic_name_);
+        AIMRT_CHECK_ERROR_THROW(subscriber_, "Get subscriber for topic '{}' failed.",
+                                topic_name_);
+
+        // 订阅消息类型（必须在Initialize环节完成）
+        bool ret = aimrt::channel::Subscribe<std_msgs::msg::String>(
+            subscriber_,
+            std::bind(&Ros2SubscriberModule::SubscriberCallback, this, std::placeholders::_1)
+        );
+        AIMRT_CHECK_ERROR_THROW(ret, "Subscribe type failed.");
+
+    } catch (const std::exception &e) {
+        AIMRT_ERROR("Init failed, {}", e.what());
+        return false;
     }
 
-  } catch (const std::exception& e) {
-    AIMRT_ERROR("Init failed, {}", e.what());
-    return false;
-  }
+    AIMRT_INFO("Init succeeded.");
 
-  AIMRT_INFO("Init succeeded.");
-
-  return true;
+    return true;
 }
 
-bool Ros2SubscriberModule::Start() {
-  AIMRT_INFO("ros2_subscriber_module Started succeeded.");
-  return true;
+bool Ros2SubscriberModule::Start()
+{
+    AIMRT_INFO("ros2_subscriber_module Started succeeded.");
+    return true;
 }
 
-void Ros2SubscriberModule::Shutdown() {
-  AIMRT_INFO("ros2_subscriber_module Shutdown succeeded.");
+void Ros2SubscriberModule::Shutdown()
+{
+    AIMRT_INFO("ros2_subscriber_module Shutdown succeeded.");
 }
 
-}  // namespace example::channel::ros2_subscriber_module
+void Ros2SubscriberModule::SubscriberCallback(const std::shared_ptr<const std_msgs::msg::String>& msg)
+{
+    AIMRT_INFO("Received message: '{}'", std_msgs::msg::to_yaml(*msg));
+}
+
+} // namespace example::channel::ros2_subscriber_module
--- WorkSpaceExample/src/channel/module/ros2_subscriber_module/ros2_subscriber_module.h
+++ WorkSpaceExample/src/channel/module/ros2_subscriber_module/ros2_subscriber_module.h
@@ -1,29 +1,38 @@
 #pragma once
 
 #include "aimrt_module_cpp_interface/module_base.h"
+#include "std_msgs/msg/string.hpp"
 
-namespace example::channel::ros2_subscriber_module {
+namespace example::channel::ros2_subscriber_module
+{
 
-class Ros2SubscriberModule : public aimrt::ModuleBase {
- public:
-  Ros2SubscriberModule() = default;
-  ~Ros2SubscriberModule() override = default;
+class Ros2SubscriberModule : public aimrt::ModuleBase
+{
+public:
+    Ros2SubscriberModule() = default;
+    ~Ros2SubscriberModule() override = default;
 
-  aimrt::ModuleInfo Info() const override {
-    return aimrt::ModuleInfo{.name = "Ros2SubscriberModule"};
-  }
+    aimrt::ModuleInfo Info() const override
+    {
+        return aimrt::ModuleInfo{.name = "Ros2SubscriberModule"};
+    }
 
-  bool Initialize(aimrt::CoreRef core) override;
+    bool Initialize(aimrt::CoreRef core) override;
 
-  bool Start() override;
+    bool Start() override;
 
-  void Shutdown() override;
+    void Shutdown() override;
 
- private:
-  auto GetLogger() { return core_.GetLogger(); }
 
- private:
-  aimrt::CoreRef core_;
+private:
+    auto GetLogger() { return core_.GetLogger(); }
+    void SubscriberCallback(const std::shared_ptr<const std_msgs::msg::String>& msg);
+
+
+private:
+    aimrt::CoreRef core_;
+    aimrt::channel::SubscriberRef subscriber_;
+    std::string topic_name_;
 };
 
-}  // namespace example::channel::ros2_subscriber_module
+} // namespace example::channel::ros2_subscriber_module
