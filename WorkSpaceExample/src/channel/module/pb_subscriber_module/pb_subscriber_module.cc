288f90469ca8ba0da05b17190586865a945fa8b8
lzh <2661006892@qq.com>
2025-08-23 21:26:54 +0800

    添加protobuf订阅者模块

--- WorkSpaceExample/src/channel/module/pb_subscriber_module/pb_subscriber_module.cc
+++ WorkSpaceExample/src/channel/module/pb_subscriber_module/pb_subscriber_module.cc
@@ -1,42 +1,56 @@
 #include "pb_subscriber_module/pb_subscriber_module.h"
-
+#include "aimrt_module_protobuf_interface/channel/protobuf_channel.h"
+#include "aimrt_module_protobuf_interface/util/protobuf_tools.h"
 #include "yaml-cpp/yaml.h"
 
 namespace example::channel::pb_subscriber_module {
 
 bool PbSubscriberModule::Initialize(aimrt::CoreRef core) {
-  // Save aimrt framework handle
-  core_ = core;
+    // Save aimrt framework handle
+    core_ = core;
 
-  try {
-    // Read cfg
-    auto file_path = core_.GetConfigurator().GetConfigFilePath();
-    if (!file_path.empty()) {
-      YAML::Node cfg_node = YAML::LoadFile(std::string(file_path));
-      for (const auto& itr : cfg_node) {
-        std::string k = itr.first.as<std::string>();
-        std::string v = itr.second.as<std::string>();
-        AIMRT_INFO("cfg [{} : {}]", k, v);
-      }
+    try {
+        // Read cfg
+        auto file_path = core_.GetConfigurator().GetConfigFilePath();
+        if (!file_path.empty()) {
+            YAML::Node cfg_node = YAML::LoadFile(std::string(file_path));
+            topic_name_ = cfg_node["channel_name"].as<std::string>();
+        }
+
+        subscriber_ = core_.GetChannelHandle().GetSubscriber(topic_name_);
+        AIMRT_CHECK_ERROR_THROW(subscriber_, "Get subscriber for topic '{}' failed.",
+                                topic_name_);
+
+        bool ret = aimrt::channel::Subscribe<aimrt::protocols::common::Header>(
+            subscriber_,
+            std::bind(&PbSubscriberModule::SubscriberCallback, this, std::placeholders::_1)
+        );
+        AIMRT_CHECK_ERROR_THROW(ret, "Subscribe type failed.");
+
+    } catch (const std::exception& e) {
+        AIMRT_ERROR("Init failed, {}", e.what());
+        return false;
     }
 
-  } catch (const std::exception& e) {
-    AIMRT_ERROR("Init failed, {}", e.what());
-    return false;
-  }
+    AIMRT_INFO("Init succeeded.");
 
-  AIMRT_INFO("Init succeeded.");
-
-  return true;
+    return true;
 }
 
 bool PbSubscriberModule::Start() {
-  AIMRT_INFO("pb_subscriber_module Started succeeded.");
-  return true;
+    AIMRT_INFO("pb_subscriber_module Started succeeded.");
+    return true;
 }
 
 void PbSubscriberModule::Shutdown() {
-  AIMRT_INFO("pb_subscriber_module Shutdown succeeded.");
+    AIMRT_INFO("pb_subscriber_module Shutdown succeeded.");
+}
+
+void PbSubscriberModule::SubscriberCallback(
+    const std::shared_ptr<const aimrt::protocols::common::Header>& msg) 
+{
+    AIMRT_INFO("In SubscriberCallback.");
+    AIMRT_INFO("Received message: '{}'", aimrt::Pb2CompactJson(*msg));   
 }
 
 }  // namespace example::channel::pb_subscriber_module
--- WorkSpaceExample/src/channel/module/pb_subscriber_module/pb_subscriber_module.h
+++ WorkSpaceExample/src/channel/module/pb_subscriber_module/pb_subscriber_module.h
@@ -1,29 +1,34 @@
 #pragma once
 
 #include "aimrt_module_cpp_interface/module_base.h"
+#include "header.pb.h"
 
 namespace example::channel::pb_subscriber_module {
 
 class PbSubscriberModule : public aimrt::ModuleBase {
- public:
-  PbSubscriberModule() = default;
-  ~PbSubscriberModule() override = default;
+public:
+    PbSubscriberModule() = default;
+    ~PbSubscriberModule() override = default;
 
-  aimrt::ModuleInfo Info() const override {
-    return aimrt::ModuleInfo{.name = "PbSubscriberModule"};
-  }
+    aimrt::ModuleInfo Info() const override {
+        return aimrt::ModuleInfo{.name = "PbSubscriberModule"};
+    }
 
-  bool Initialize(aimrt::CoreRef core) override;
+    bool Initialize(aimrt::CoreRef core) override;
+    
+    bool Start() override;
 
-  bool Start() override;
+    void Shutdown() override;
 
-  void Shutdown() override;
+private:
+    auto GetLogger() { return core_.GetLogger(); }
+    void SubscriberCallback(const std::shared_ptr<const aimrt::protocols::common::Header>& msg);
 
- private:
-  auto GetLogger() { return core_.GetLogger(); }
+private:
+    aimrt::CoreRef core_;
+    aimrt::channel::SubscriberRef subscriber_;
 
- private:
-  aimrt::CoreRef core_;
+    std::string topic_name_;
 };
 
 }  // namespace example::channel::pb_subscriber_module
