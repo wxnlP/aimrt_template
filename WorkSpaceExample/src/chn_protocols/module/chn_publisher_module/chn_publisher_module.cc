4951f1059e8b2f81d0251134b685c9736766d0b8
lzh <2661006892@qq.com>
2025-08-24 21:39:52 +0800

    添加自定义protobuf协议发布者模块

--- WorkSpaceExample/src/chn_protocols/module/chn_publisher_module/chn_publisher_module.cc
+++ WorkSpaceExample/src/chn_protocols/module/chn_publisher_module/chn_publisher_module.cc
@@ -1,42 +1,86 @@
 #include "chn_publisher_module/chn_publisher_module.h"
-
+#include "aimrt_module_protobuf_interface/channel/protobuf_channel.h"
+#include "aimrt_module_protobuf_interface/util/protobuf_tools.h"
+#include "info.pb.h"
 #include "yaml-cpp/yaml.h"
 
 namespace example::chn_protocols::chn_publisher_module {
 
 bool ChnPublisherModule::Initialize(aimrt::CoreRef core) {
-  // Save aimrt framework handle
-  core_ = core;
+    // Save aimrt framework handle
+    core_ = core;
 
-  try {
-    // Read cfg
-    auto file_path = core_.GetConfigurator().GetConfigFilePath();
-    if (!file_path.empty()) {
-      YAML::Node cfg_node = YAML::LoadFile(std::string(file_path));
-      for (const auto& itr : cfg_node) {
-        std::string k = itr.first.as<std::string>();
-        std::string v = itr.second.as<std::string>();
-        AIMRT_INFO("cfg [{} : {}]", k, v);
-      }
+    try {
+        // Read cfg
+        auto file_path = core_.GetConfigurator().GetConfigFilePath();
+        if (!file_path.empty()) {
+            YAML::Node cfg_node = YAML::LoadFile(std::string(file_path));
+            topic_name_ = cfg_node["channel_name"].as<std::string>();
+            channel_frq_ = cfg_node["channel_frq"].as<double>();
+        }
+
+        pub_executor_ = core_.GetExecutorManager().GetExecutor("pub_executor");
+        AIMRT_CHECK_ERROR_THROW(pub_executor_ && pub_executor_.SupportTimerSchedule(),
+                                "can not get a time enable executor.");
+        
+                                // 获取发布者句柄
+        publisher_pb_ = core_.GetChannelHandle().GetPublisher(topic_name_);
+        AIMRT_CHECK_ERROR_THROW(publisher_pb_, "Get publisher {} failed.", 
+                                topic_name_);
+                        
+        // 注册发布类型
+        bool ret = aimrt::channel::RegisterPublishType<
+                                example::protocols::test::Info>(publisher_pb_);
+        AIMRT_CHECK_ERROR_THROW(ret, "Register publish type failed.");
+
+    } catch (const std::exception& e) {
+        AIMRT_ERROR("Init failed, {}", e.what());
+        return false;
     }
 
-  } catch (const std::exception& e) {
-    AIMRT_ERROR("Init failed, {}", e.what());
-    return false;
-  }
+    AIMRT_INFO("Init succeeded.");
 
-  AIMRT_INFO("Init succeeded.");
-
-  return true;
+    return true;
 }
 
-bool ChnPublisherModule::Start() {
-  AIMRT_INFO("chn_publisher_module Started succeeded.");
-  return true;
+bool ChnPublisherModule::Start() 
+{
+    run_flag_ = true;
+    pub_executor_.Execute(std::bind(&ChnPublisherModule::MainLoop, this));
+    AIMRT_INFO("chn_publisher_module Started succeeded.");
+    return true;
 }
 
-void ChnPublisherModule::Shutdown() {
-  AIMRT_INFO("chn_publisher_module Shutdown succeeded.");
+void ChnPublisherModule::Shutdown() 
+{
+    run_flag_ = false;
+    stop_sig_.get_future().wait();
+    AIMRT_INFO("chn_publisher_module Shutdown succeeded.");
 }
+
+void ChnPublisherModule::MainLoop() 
+{
+    try {
+        example::protocols::test::Info msg;
+        msg.set_info("I am a info.");
+        // 主循环
+        while (run_flag_)
+        {
+            msg.set_time_stamp(std::time(0));
+            aimrt::channel::Publish(publisher_pb_, msg);
+
+            AIMRT_INFO("Publish msg: {}", aimrt::Pb2CompactJson(msg));
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(
+                                        static_cast<uint32_t>(1000/channel_frq_)));
+        }
+        AIMRT_INFO("ChnPublisherModule MainLoop exit normally.");
+
+    } catch (std::exception& e) {
+        AIMRT_ERROR("MainLoop get exception: {}", e.what());
+    }
+    stop_sig_.set_value();
+}
+
 
 }  // namespace example::chn_protocols::chn_publisher_module
--- WorkSpaceExample/src/chn_protocols/module/chn_publisher_module/chn_publisher_module.h
+++ WorkSpaceExample/src/chn_protocols/module/chn_publisher_module/chn_publisher_module.h
@@ -5,25 +5,34 @@
 namespace example::chn_protocols::chn_publisher_module {
 
 class ChnPublisherModule : public aimrt::ModuleBase {
- public:
-  ChnPublisherModule() = default;
-  ~ChnPublisherModule() override = default;
+   public:
+    ChnPublisherModule() = default;
+    ~ChnPublisherModule() override = default;
 
-  aimrt::ModuleInfo Info() const override {
-    return aimrt::ModuleInfo{.name = "ChnPublisherModule"};
-  }
+    aimrt::ModuleInfo Info() const override {
+        return aimrt::ModuleInfo{.name = "ChnPublisherModule"};
+    }
 
-  bool Initialize(aimrt::CoreRef core) override;
+    bool Initialize(aimrt::CoreRef core) override;
 
-  bool Start() override;
+    bool Start() override;
 
-  void Shutdown() override;
+    void Shutdown() override;
 
- private:
-  auto GetLogger() { return core_.GetLogger(); }
+   private:
+    auto GetLogger() { return core_.GetLogger(); }
+    void MainLoop();
 
- private:
-  aimrt::CoreRef core_;
+   private:
+    aimrt::CoreRef core_;
+    aimrt::executor::ExecutorRef pub_executor_;
+    aimrt::channel::PublisherRef publisher_pb_;
+    aimrt::channel::PublisherRef publisher_ros2_;
+
+    std::string topic_name_;
+    double channel_frq_;
+    bool run_flag_ = false;
+    std::promise<void> stop_sig_;
 };
 
 }  // namespace example::chn_protocols::chn_publisher_module
