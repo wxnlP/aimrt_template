308d6389a14178ebb2c7cef7d11e1ab7b0f85de5
lzh <2661006892@qq.com>
2025-08-24 21:40:08 +0800

    添加自定义protobuf协议订阅者模块

--- WorkSpaceExample/src/chn_protocols/module/chn_subscriber_module/chn_subscriber_module.cc
+++ WorkSpaceExample/src/chn_protocols/module/chn_subscriber_module/chn_subscriber_module.cc
@@ -1,42 +1,58 @@
 #include "chn_subscriber_module/chn_subscriber_module.h"
+#include "aimrt_module_protobuf_interface/channel/protobuf_channel.h"
+#include "aimrt_module_protobuf_interface/util/protobuf_tools.h"
 
 #include "yaml-cpp/yaml.h"
 
 namespace example::chn_protocols::chn_subscriber_module {
 
 bool ChnSubscriberModule::Initialize(aimrt::CoreRef core) {
-  // Save aimrt framework handle
-  core_ = core;
+    // Save aimrt framework handle
+    core_ = core;
 
-  try {
-    // Read cfg
-    auto file_path = core_.GetConfigurator().GetConfigFilePath();
-    if (!file_path.empty()) {
-      YAML::Node cfg_node = YAML::LoadFile(std::string(file_path));
-      for (const auto& itr : cfg_node) {
-        std::string k = itr.first.as<std::string>();
-        std::string v = itr.second.as<std::string>();
-        AIMRT_INFO("cfg [{} : {}]", k, v);
-      }
+    try {
+        // Read cfg
+        auto file_path = core_.GetConfigurator().GetConfigFilePath();
+        if (!file_path.empty()) {
+            YAML::Node cfg_node = YAML::LoadFile(std::string(file_path));
+            topic_name_ = cfg_node["channel_name"].as<std::string>();
+        }
+        subscriber_pb_ = core_.GetChannelHandle().GetSubscriber(topic_name_);
+        AIMRT_CHECK_ERROR_THROW(subscriber_pb_, 
+            "Get subscriber for topic '{}' failed.", topic_name_);
+
+        bool ret = aimrt::channel::Subscribe<example::protocols::test::Info>(
+            subscriber_pb_,
+            std::bind(
+                &ChnSubscriberModule::PbSubscriberCallback, 
+                this, 
+                std::placeholders::_1));
+        AIMRT_CHECK_ERROR_THROW(ret, "Subscribe type failed.");
+
+    } catch (const std::exception& e) {
+        AIMRT_ERROR("Init failed, {}", e.what());
+        return false;
     }
 
-  } catch (const std::exception& e) {
-    AIMRT_ERROR("Init failed, {}", e.what());
-    return false;
-  }
+    AIMRT_INFO("Init succeeded.");
 
-  AIMRT_INFO("Init succeeded.");
-
-  return true;
+    return true;
 }
 
 bool ChnSubscriberModule::Start() {
-  AIMRT_INFO("chn_subscriber_module Started succeeded.");
-  return true;
+    AIMRT_INFO("chn_subscriber_module Started succeeded.");
+    return true;
 }
 
 void ChnSubscriberModule::Shutdown() {
-  AIMRT_INFO("chn_subscriber_module Shutdown succeeded.");
+    AIMRT_INFO("chn_subscriber_module Shutdown succeeded.");
+}
+
+void ChnSubscriberModule::PbSubscriberCallback(
+    const std::shared_ptr<const example::protocols::test::Info>& msg) 
+{
+    AIMRT_INFO("In PbSubscriberCallback.");
+    AIMRT_INFO("Received message: '{}'", aimrt::Pb2CompactJson(*msg));  
 }
 
 }  // namespace example::chn_protocols::chn_subscriber_module
