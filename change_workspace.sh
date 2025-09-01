3921fa930eb51b203df36d417bf071cef7d73722
lzh <2661006892@qq.com>
2025-09-01 15:22:19 +0800

    升级工作空间转移脚本

--- change_workspace.sh
+++ change_workspace.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-if [ $# -eq 0 ]; then
-    echo "err 未提供参数！"
-    exit 1
-elif [ $# -lt 2 ]; then
-    echo "err 至少需要2个参数！"
+# 参数检查
+if [ $# -ne 3 ]; then
+    echo "用法: $0 <项目名> <工作空间路径> <命名空间前缀>"
     exit 1
 fi
 
 PJ_NAME=${1#./}
 WK_SPACE=${2#./}
+NAMESPACE=${3#./}
 
 echo "开始工作空间转移......"
 
@@ -29,6 +28,12 @@ fi
 
 cd ${WK_SPACE}/src/${PJ_NAME}/
 sed -i "2s/set_root_namespace(\"${PJ_NAME}\")/set_namespace()/" CMakeLists.txt
+# 文本替换处理
+find "./module" -type f \( -name "*.cc" -o -name "*.cpp" -o -name "*.h" -o -name "*.hpp" \) \
+  -exec sed -i.bak "s/${PJ_NAME}/${NAMESPACE}::${PJ_NAME}/g" {} +
+find "./pkg" -type f \( -name "*.cc" -o -name "*.cpp" -o -name "*.h" -o -name "*.hpp" \) \
+  -exec sed -i.bak "s/${PJ_NAME}/${NAMESPACE}::${PJ_NAME}/g" {} +
+
 cd ../
 sed -i "\$a add_subdirectory(${PJ_NAME})" CMakeLists.txt
 
